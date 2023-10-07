15221ba74fef1670d239044f389bdadbf5cac7ea
zoreu <joel_webmaster@live.com>
2023-10-06 22:26:25 -0300

    update

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-    return 'Pagina de Joel, em construcao'
+    return 'Pagina de Joel'
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=80)
