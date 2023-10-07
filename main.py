76f305e34608d988b19f0a1c451718023bc04022
zoreu <joel_webmaster@live.com>
2023-10-06 22:19:32 -0300

    teste

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     #return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-    return 'Pagina de Joel'
+    return 'Pagina de Joel, em construcao'
 
 
 if __name__ == '__main__':
